7ac5e2ae53b267d34676217c7c1cdfcaa89be101
Pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2023-12-20 01:26:04 +0530

    ques 1

--- valid_parenthesis.c
+++ valid_parenthesis.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(){
+#include <stdbool.h>
+
+bool isValid(char* s) {
+    char *stack = malloc(strlen(s));
+    assert(stack);
+    int top =0;
+    int i;
+    int c, e;
+    bool ret = false;
+
+    for(int i=0; i<strlen(s); i++) {
+        c = s[i];
+        if(c == ')'){
+            if(top == 0){
+                goto out;
+            }
+            e = stack[--top];
+            if(e != '(')
+                goto out;
+        }
+        else if(c == '}'){
+            if(top == 0){
+                goto out;
+            }
+            e = stack[--top];
+            if(e != '{')
+                goto out;
+        }
+        else if(c == ']'){
+            if(top == 0){
+                goto out;
+            }
+            e = stack[--top];
+            if(e != '[')
+                goto out;
+        }
+        else{
+            stack[top++] = c;
+        }
+    }
+
+    ret = top == 0;
+out:
+    free(stack);
+    return ret;
     
-    return 0;
 }
